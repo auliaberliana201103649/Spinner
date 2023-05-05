7f6c5d3e73c2e296d820a89cbaca66043363bcff
AuliaBerliana201103649 <auliaaberliana@gmail.com>
2023-05-05 14:08:58 +0700

    prima

--- app/src/main/java/com/auliaberliana201103649/spinner/MainActivity.java
+++ app/src/main/java/com/auliaberliana201103649/spinner/MainActivity.java
@@ -8,27 +8,29 @@ import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import android.widget.ArrayAdapter;
 public class MainActivity extends AppCompatActivity {
-    private Spinner _spinner1, _spinner2, _spinner3, _spinner4;
+    private Spinner _spinner1, _spinner2, _spinner3, prime_spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        _spinner1 = (Spinner)findViewById(R.id.spinner1);
-        _spinner2 = (Spinner)findViewById(R.id.spinner2);
-        _spinner3 = (Spinner)findViewById(R.id.spinner3);
-        _spinner4 = (Spinner)findViewById(R.id.spinner4);
+        _spinner1 = (Spinner) findViewById(R.id.spinner1);
+        _spinner2 = (Spinner) findViewById(R.id.spinner2);
+        _spinner3 = (Spinner) findViewById(R.id.spinner3);
+        prime_spinner = (Spinner) findViewById(R.id.prime_spinner);
 
         initSpinner1();
         initSpinner2();
         initSpinner3();
-        initSpinner4();
+        prime_spinner();
+
+
     }
 
-    private void initSpinner1(){
+    private void initSpinner1() {
         List<String> buahList = new ArrayList<>();
         buahList.add("Rambutan");
         buahList.add("Langsat");
@@ -36,40 +38,54 @@ public class MainActivity extends AppCompatActivity {
         buahList.add("Nangka");
         buahList.add("Mangga");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,buahList);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, buahList);
         _spinner1.setAdapter(arrayAdapter);
     }
 
-    private void initSpinner2(){
+    private void initSpinner2() {
         List<String> mahasiswaList = new ArrayList<>();
 
-        for (int i = 1; i <= 1000; i++){
+        for (int i = 1; i <= 1000; i++) {
             mahasiswaList.add("Mahasiswa ke-" + i);
         }
 
-        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,mahasiswaList);
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mahasiswaList);
         _spinner2.setAdapter(arrayAdapter2);
     }
 
-    private void initSpinner3(){
+    private void initSpinner3() {
         List<String> ganjilList = new ArrayList<>();
 
-        for (int i = 1; i <= 999; i+=2){
+        for (int i = 1; i <= 999; i += 2) {
             ganjilList.add("Bilangan ke-" + i);
         }
 
-        ArrayAdapter<String> arrayAdapter3 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,ganjilList);
+        ArrayAdapter<String> arrayAdapter3 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ganjilList);
         _spinner3.setAdapter(arrayAdapter3);
     }
 
-    private void initSpinner4(){
-        List<String> genapList = new ArrayList<>();
+    private void prime_spinner() {
+        Spinner primeSpinner = findViewById(R.id.prime_spinner);
 
-        for (int i = 2; i <= 1000; i+=2){
-            genapList.add("Bilangan ke-" + i);
+        ArrayList<Integer> primeNumbers = new ArrayList<>();
+        for (int i = 2; i <= 100; i++) {
+
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primeNumbers.add(i);
+            }
+
+
+            ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this,
+                    android.R.layout.simple_spinner_item, primeNumbers);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            primeSpinner.setAdapter(adapter);
         }
-
-        ArrayAdapter<String> arrayAdapter4 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,genapList);
-        _spinner4.setAdapter(arrayAdapter4);
     }
 }
---
+++ app/src/main/java/com/auliaberliana201103649/spinner/integer.java
@@ -0,0 +1,4 @@
+package com.auliaberliana201103649.spinner;
+
+public class integer {
+}
